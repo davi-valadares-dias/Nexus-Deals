383892b508cbdadf5fd03d25308df697da944f1b
Davi Valadares Dias <davivaladarescontato@gmail.com>
2026-09-15 19:28:30 -0300

    Adiciona estrututa Spring Boot (Web, JPA, H2, Validation)

---
+++ src/main/java/com/nexusdelas/nexus_deals/NexusDealsApplication.java
@@ -0,0 +1,13 @@
+package com.nexusdelas.nexus_deals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusDealsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexusdelas/nexus_deals/NexusDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusdelas.nexus_deals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
