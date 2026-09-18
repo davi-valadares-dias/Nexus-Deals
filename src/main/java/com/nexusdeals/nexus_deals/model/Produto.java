69145ac780b81d315996f8df3c8ecd09a341b11a
Davi Valadares Dias <davivaladarescontato@gmail.com>
2026-09-17 21:37:07 -0300

    fix: corrige setters de Produto com parametros incorretos

--- src/main/java/com/nexusdeals/nexus_deals/model/Produto.java
+++ src/main/java/com/nexusdeals/nexus_deals/model/Produto.java
@@ -31,43 +31,43 @@ public class Produto {
     public Long getId(){
         return id;
     }
-    public void setId(Long Id){
+    public void setId(Long id){
         this.id = id;
     }
     public String getNome(){
         return nome;
     }
-    public void setNome(String Nome){
+    public void setNome(String nome){
         this.nome = nome;
     }
     public String getMarca(){
         return marca;
     }
-    public void setMarca(String Marca){
+    public void setMarca(String marca){
         this.marca = marca;
     }
     public String getCategoria(){
         return categoria;
     }
-    public void setCategoria(){
+    public void setCategoria(String categoria){
         this.categoria = categoria;
     }
     public String getSku(){
         return sku;
     }
-    public void setSku(String Sku){
+    public void setSku(String sku){
         this.sku = sku;
     }
     public String getEan(){
         return ean;
     }
-    public void setEan(String Ean){
+    public void setEan(String ean){
         this.ean = ean;
     }
     public String getImagemUrl(){
         return imagemUrl;
     }
-    public void setImagemUrl(String ImagemUrl){
+    public void setImagemUrl(String imagemUrl){
         this.imagemUrl = imagemUrl;
     }
 }
